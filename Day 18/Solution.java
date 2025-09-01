1c6f391b1cde3ca75b56ce0396da8320b21c6653
vaibhav <gatlewarvaibhav@gmail.com>
2025-09-01 21:04:50 +0530

    Day 18

---
+++ Day 18/Solution.java
@@ -0,0 +1,21 @@
+
+public class Solution {
+    public static int count(int N) {
+        int count = 0;
+        for (int i = 1; i <= Math.sqrt(N); i++) {
+            if (N % i == 0) {
+                count++;
+                if (i != N / i) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int N = 12;
+        int result = count(N);
+        System.out.println(result);
+    }
+}
